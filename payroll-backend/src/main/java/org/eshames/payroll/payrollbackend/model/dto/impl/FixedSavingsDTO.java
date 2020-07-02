83f3b6d53243f5b170f2820d85a76494d92c78b0
eliav.shames <eliav.shames@operative.com>
2020-07-02 22:20:55 +0300

    working on BL

--- payroll-backend/src/main/java/org/eshames/payroll/payrollbackend/model/dto/impl/FixedSavingsDTO.java
+++ payroll-backend/src/main/java/org/eshames/payroll/payrollbackend/model/dto/impl/FixedSavingsDTO.java
@@ -1,7 +1,9 @@
 package org.eshames.payroll.payrollbackend.model.dto.impl;
 
 import org.eshames.payroll.payrollbackend.model.dto.SavingsDTO;
+import org.springframework.stereotype.Component;
 
+@Component("savings_fixed")
 public class FixedSavingsDTO implements SavingsDTO {
     @Override
     public float calculateSavings(float savingsFactor, float grossIncome) {
--- payroll-backend/src/main/java/org/eshames/payroll/payrollbackend/model/dto/impl/PercentageSavingsDTO.java
+++ payroll-backend/src/main/java/org/eshames/payroll/payrollbackend/model/dto/impl/PercentageSavingsDTO.java
@@ -1,7 +1,9 @@
 package org.eshames.payroll.payrollbackend.model.dto.impl;
 
 import org.eshames.payroll.payrollbackend.model.dto.SavingsDTO;
+import org.springframework.stereotype.Component;
 
+@Component("savings_percentage")
 public class PercentageSavingsDTO implements SavingsDTO {
 
     @Override
