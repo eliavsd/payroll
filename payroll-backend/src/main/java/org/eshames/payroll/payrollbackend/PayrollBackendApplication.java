58f1ccefd58dd2a6745158fbc1020736149c27ce
eliav.shames <eliav.shames@operative.com>
2020-07-02 20:05:17 +0300

    frontend and backend created

---
+++ payroll-backend/src/main/java/org/eshames/payroll/payrollbackend/PayrollBackendApplication.java
@@ -0,0 +1,13 @@
+package org.eshames.payroll.payrollbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollBackendApplication.class, args);
+	}
+
+}
---
+++ payroll-backend/src/test/java/org/eshames/payroll/payrollbackend/PayrollBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eshames.payroll.payrollbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
