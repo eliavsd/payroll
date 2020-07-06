886b5daa5238d56e6f19fe7ac4819204175b2ce6
eliav.shames <eliav.shames@operative.com>
2020-07-06 22:13:42 +0300

    begin work on frontend, create dtos

--- payroll-backend/src/main/java/org/eshames/payroll/payrollbackend/model/constants/TaxConstants.java
+++ payroll-backend/src/main/java/org/eshames/payroll/payrollbackend/model/constants/TaxConstants.java
@@ -1,8 +1,5 @@
 package org.eshames.payroll.payrollbackend.model.constants;
 
-import java.util.Arrays;
-import java.util.List;
-
 public final class TaxConstants {
     public static final int TAX_POINT_VALUE = 219;
     public static final int EDU_FUND_MAX_EXEMPTION = 15712;
