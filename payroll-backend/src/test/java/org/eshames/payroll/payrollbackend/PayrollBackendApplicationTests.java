bc4d1a8e12e9b0c5a842737d9edca045792b3f6b
eliav.shames <eliav.shames@operative.com>
2020-07-15 21:32:12 +0300

    fixed empty tests

--- payroll-backend/src/test/java/org/eshames/payroll/payrollbackend/PayrollBackendApplicationTests.java
+++ payroll-backend/src/test/java/org/eshames/payroll/payrollbackend/PayrollBackendApplicationTests.java
@@ -1,9 +1,10 @@
 package org.eshames.payroll.payrollbackend;
 
+import org.eshames.payroll.payrollbackend.app.PayrollBackendApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PayrollBackendApplication.class)
 class PayrollBackendApplicationTests {
 
 	@Test
